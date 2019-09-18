ff6c7a34e446f1d9054147283a1e3d1cf54911d4
chenqixian <chenqixian@buaa.edu.cn>
2019-09-18 10:18:16 +0800

    GCD & LCM

---
+++ 4-MathProblems/eg4-5LCM.cpp
@@ -0,0 +1,18 @@
+/*
+	最小公倍数
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b) {
+	if(b == 0) return a;
+	return gcd(b, a % b);
+}
+
+int main(void) {
+	int a, b;
+	while(scanf("%d%d", &a, &b) != EOF) {
+		printf("%d\n", a * b / gcd(a, b));
+	}
+	return 0;
+}
