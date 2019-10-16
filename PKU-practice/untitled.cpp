183e1c6f2724dde1ab06036093382eac6a19909b
chenqixian <chenqixian@buaa.edu.cn>
2019-10-16 15:41:00 +0800

    delete unwanted

--- PKU-practice/untitled.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-#define isLeap(x) x % 100 != 0 && x % 4 == 0 || x % 400 == 0
-
-int main(void) {
-	int n;
-	scanf("%d", &n);
-	printf("%d\n", isLeap(n)? 1 : 0);
-	return 0;
-}
