95896fe4b2e6b740df3270e7542f2c44adfadc7c
chenqixian <chenqixian@buaa.edu.cn>
2019-11-07 10:44:48 +0800

    日期类

---
+++ NowCoder-practice/NC-21.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	int m,n,x;
+	int a[20];
+	while(scanf("%d", &m) != EOF) {
+		while(m --) {
+			scanf("%d", &n);
+			for(int i = 0 ; i <= n ; i ++) {
+				scanf("%d", &a[i]);
+			}
+			scanf("%d", &x);
+			long long ans = 0;
+			for(int i = 0 ; i <= n ; i ++) {
+				ans += a[i] * pow(x, i);
+			}
+			printf("%lld\n", ans);
+		}
+	}
+	return 0;
+}
